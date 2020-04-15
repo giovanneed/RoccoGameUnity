ff7e47fca5def1bd16f5344efeb5d7eb1624ea95
Giovanne <giovanneed@gmail.com>
2020-04-15 16:00:37 -0400

    initail

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+
+
+	public float distance = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        
+    }
+
+    public void Hello(){
+    	Debug.Log("hello camera");
+    	distance = 2f;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {	
+    	distance = distance  + 0.04f;
+
+    	transform.position = new Vector3(distance, 1,-10);  
+        
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Player : MonoBehaviour
+{
+
+	public Rigidbody2D rb;
+	public Transform groundCheck;
+	public float groundCheckRadius;
+	public LayerMask whatIsGround;
+	public bool onGround;
+
+
+    public Text distanceLabel; 
+
+    public int distance = 0;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        this.updateDistance();
+
+    }
+
+    void updateDistance(){
+
+        distanceLabel.text = "Distance "  + distance + " m";
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+     	rb.velocity = new Vector2(3, rb.velocity.y);   
+        onGround = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+     	if (Input.GetKey(KeyCode.Space)  && onGround)
+        {
+                rb.velocity = new Vector2(rb.velocity.x, 5);
+        }
+
+        distance = distance  + 1;
+        this.updateDistance();
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+{
+    if (collision.gameObject.tag == "Enemy")
+    {
+        rb.transform.position = new Vector2(-5, -3);  
+        GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>().Hello();
+        distance = 0;     
+        this.updateDistance();
+
+
+    }
+}
+}
+
+
